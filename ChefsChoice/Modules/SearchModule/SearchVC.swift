67488483c83c92f31634abc936d2ab6c3a989191
MrMurman <74971676+MrMurman@users.noreply.github.com>
2023-03-04 14:20:17 +0300

    Now with updated UI

--- ChefsChoice/Modules/SearchModule/SearchVC.swift
+++ ChefsChoice/Modules/SearchModule/SearchVC.swift
@@ -39,7 +39,7 @@ class SearchVC: UIViewController {
 
     var categoryStackView                  = UIStackView()
     var categoryTitleLabel                 = UILabel()
-    //var categoryGrid                       = UICollectionView()
+    //var categoryGrid                     = UICollectionView()
     var gridPlaceholder                    = UIView()
     
     var cookingTimeStack                   = UIStackView()
@@ -64,6 +64,9 @@ class SearchVC: UIViewController {
         
         setupUI()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
     
     
@@ -77,105 +80,28 @@ class SearchVC: UIViewController {
             configureTitleLabels(label: key, withTitle: value)
         }
         
+        
         setupFilterStackView()
         configureTableView()
-    }
-    
-    func configureTitleLabels(label: UILabel, withTitle title: String) {
         
-        view.addSubview(label)
-        
-        label.text = title
-        label.font = UIFont.systemFont(ofSize: 30, weight: .bold)
-        label.textColor = .label
-        label.translatesAutoresizingMaskIntoConstraints = false
-    }
-    
-    func сonfigureRatingButtons() {
-        
-        view.addSubview(ratingButton)
-        
-        ratingButton.setBackgroundImage(UIImage(systemName: "star"), for: .normal)
-        ratingButton.setBackgroundImage(UIImage(systemName: "star.fill"), for: .selected)
-        
-        ratingButton.clipsToBounds = true
-        ratingButton.layer.borderWidth = 1
-        ratingButton.layer.borderColor = UIColor.black.cgColor
-        
-        ratingButton.translatesAutoresizingMaskIntoConstraints = false
-    }
-    
-    func configureApplyButton() {
-        
-        view.addSubview(applyButton)
-        
-        applyButton.setTitle("Apply", for: .normal)
-        
-        applyButton.translatesAutoresizingMaskIntoConstraints = false
-    }
-    
-    func setupRatingStackView() {
-        
-        configureApplyButton()
-        
-        let hStack = UIStackView()
-        view.addSubview(hStack)
-        hStack.addArrangedSubview(ratingTitleLabel)
-        hStack.addArrangedSubview(applyButton)
-        hStack.translatesAutoresizingMaskIntoConstraints = false
-        
-        let starStack = UIStackView()
-        view.addSubview(starStack)
-        
-    //TODO: find out how to better add buttons to stack
-        for _ in 1...5 {
-            starStack.addArrangedSubview(ratingButton)
-        }
-        starStack.translatesAutoresizingMaskIntoConstraints = false
-        
-    }
-    
-    func configureGridView() {
-        view.addSubview(gridPlaceholder)
-        gridPlaceholder.backgroundColor = .black
-        gridPlaceholder.translatesAutoresizingMaskIntoConstraints = false
-    }
-    
-    func setupCategoryStackView() {
-        view.addSubview(categoryStackView)
-        
-        categoryStackView.addArrangedSubview(categoryTitleLabel)
-        categoryStackView.addArrangedSubview(gridPlaceholder)
-        
-        //TODO: Add proper Grid
-        categoryStackView.translatesAutoresizingMaskIntoConstraints = false
-    }
-    
-    func configureCookingTimeSlider() {
-        
-    }
-    
-    func configureCookingTimeLabel(){
-        
-    }
-    
-    func setupCookingTimeStackView() {
-        
-        configureCookingTimeLabel()
-        configureCookingTimeSlider()
-    }
-    
-    func configureHealthyTypeButtons() {
-        
-    }
-    
-    func setupHealthyTypeStackView() {
-        
-        configureHealthyTypeButtons()
+        filterTitleLabel.textAlignment = .center
+        filterTitleLabel.font = UIFont.systemFont(ofSize: 15, weight: .black)
     }
     
     
+    // sets up main stack view
     func setupFilterStackView() {
+        
+        filterStackView.layer.borderColor = UIColor.black.cgColor
+        filterStackView.layer.borderWidth = 1
+        filterStackView.layer.cornerRadius = 20
+        
+        filterStackView.axis = .vertical
+        filterStackView.spacing = 20
+        filterStackView.alignment = .fill
+        
+        filterStackView.isLayoutMarginsRelativeArrangement = true
+        filterStackView.layoutMargins = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
         
         view.addSubview(filterStackView)
         
@@ -185,7 +111,9 @@ class SearchVC: UIViewController {
         setupHealthyTypeStackView()
         
         
+        filterStackView.addArrangedSubview(filterTitleLabel)
         filterStackView.addArrangedSubview(ratingStackView)
+                //filterStackView.addArrangedSubview(ratingButton)
         filterStackView.addArrangedSubview(categoryStackView)
         filterStackView.addArrangedSubview(cookingTimeStack)
         filterStackView.addArrangedSubview(healthyTypeStackView)
@@ -194,18 +122,221 @@ class SearchVC: UIViewController {
         filterStackView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             filterStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            filterStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            filterStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            filterStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor,constant: -20)
         ])
     }
     
-    func configureTableView() {
+    
+    
+    func configureTitleLabels(label: UILabel, withTitle title: String) {
         
+        view.addSubview(label)
+        
+        label.text = title
+        label.font = UIFont.systemFont(ofSize: 15, weight: .bold)
+        label.textColor = .label
+        label.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func сonfigureRatingButtons(for button: UIButton) {
+        
+        view.addSubview(button)
+        
+        button.setBackgroundImage(UIImage(systemName: "star"), for: .normal)
+        button.setBackgroundImage(UIImage(systemName: "star.fill"), for: .selected)
+        
+        button.tintColor = .systemOrange
+    
+    
+      
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            button.widthAnchor.constraint(equalToConstant: 50),
+            button.heightAnchor.constraint(equalToConstant: 50),
+        ])
+    }
+    
+    func configureApplyButton() {
+        
+        //view.addSubview(applyButton)
+        
+        applyButton.setTitle("Apply", for: .normal)
+        applyButton.setTitleColor(.blue, for: .normal)
+        
+        applyButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            applyButton.heightAnchor.constraint(equalToConstant: 20),
+            applyButton.widthAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    func setupRatingStackView() {
+        
+        configureApplyButton()
+        //сonfigureRatingButtons()
+        
+        ratingStackView.axis = .vertical
+        //ratingStackView.alignment = .trailing
+        ratingStackView.distribution = .equalSpacing
+        
+        let hStack = UIStackView()
+        view.addSubview(hStack)
+        hStack.axis = .horizontal
+        
+        hStack.addArrangedSubview(ratingTitleLabel)
+        hStack.addArrangedSubview(applyButton)
+        hStack.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        
+        let starStack = UIStackView()
+        starStack.axis = .horizontal
+        starStack.distribution = .equalSpacing
+        view.addSubview(starStack)
+        
+        
+        
+    //TODO: find out how to better add buttons to stack
+        for _ in 1...5 {
+            let newStarBTN = UIButton()
+            сonfigureRatingButtons(for: newStarBTN)
+            view.addSubview(newStarBTN)
+            starStack.addArrangedSubview(newStarBTN)
+        }
+        starStack.translatesAutoresizingMaskIntoConstraints = false
+        
+        ratingStackView.addArrangedSubview(hStack)
+        ratingStackView.addArrangedSubview(starStack)
+    }
+    
+    func configureGridView() {
+        view.addSubview(gridPlaceholder)
+        gridPlaceholder.backgroundColor = .gray
+        gridPlaceholder.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            gridPlaceholder.widthAnchor.constraint(equalToConstant: 100),
+            gridPlaceholder.heightAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    func setupCategoryStackView() {
+        configureGridView()
+        
+        view.addSubview(categoryStackView)
+
+        categoryStackView.axis = .vertical
+        categoryStackView.addArrangedSubview(categoryTitleLabel)
+        categoryStackView.addArrangedSubview(gridPlaceholder)
+
+        //TODO: Add proper Grid
+        categoryStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+           
+            categoryStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
+    func configureCookingTimeSlider() {
+        view.addSubview(cookingTimeSlider)
+        
+        // further customisation
+    }
+
+    func configureCookingTimeLabel(){
+        view.addSubview(cookingTimeLabel)
+        
+        cookingTimeLabel.text = "1 H 20 M"
+        cookingTimeLabel.font = UIFont.systemFont(ofSize: 20)
+        cookingTimeLabel.textColor = .label
+        cookingTimeLabel.textAlignment = .center
+    }
+
+    func setupCookingTimeStackView() {
+
+        configureCookingTimeLabel()
+        configureCookingTimeSlider()
+        
+        cookingTimeStack.axis = .vertical
+        cookingTimeStack.spacing = 10
+        
+        cookingTimeStack.addArrangedSubview(cookingTitleLabel)
+        cookingTimeStack.addArrangedSubview(cookingTimeSlider)
+        cookingTimeStack.addArrangedSubview(cookingTimeLabel)
+        
+    }
+
+    func configureHealthyTypeButtons() {
+        view.addSubview(healthyTypeButton)
+        
+        healthyTypeButton.setTitle("Healthy", for: .normal)
+        healthyTypeButton.setTitleColor(.label, for: .normal)
+    }
+
+    func setupHealthyTypeStackView() {
+
+        configureHealthyTypeButtons()
+        
+        healthyTypeStackView.axis = .vertical
+        
+        healthyTypeStackView.addArrangedSubview(healthyTypeTitleLabel)
+        healthyTypeStackView.addArrangedSubview(healthyTypeButton)
+    }
+    
+    
+}
+
+
+//MARK: - TableView Setup
+
+extension SearchVC: UITableViewDelegate, UITableViewDataSource {
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? RecipeTableViewCell {
+            
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        120
+    }
+    
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        "Search results"
+    }
+    func configureTableView() {
+
         view.addSubview(tableView)
         
+        tableView.largeContentTitle = "Search results"
+        tableView.register(RecipeTableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        tableView.layer.borderWidth = 1
+        tableView.layer.cornerRadius = 20
+        tableView.layer.borderColor = UIColor.black.cgColor
+        tableView.backgroundColor = .cyan
+
         tableView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: filterStackView.bottomAnchor, constant: 20),
-            tableView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+           tableView.topAnchor.constraint(equalTo: filterStackView.bottomAnchor, constant: 20),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
     
