5ac77117dac6f88d820417074041c82ea55805cc
MrMurman <74971676+MrMurman@users.noreply.github.com>
2023-03-05 10:03:30 +0300

    Added animation to filter stack + finished collection grid

--- ChefsChoice/Modules/SearchModule/SearchVC.swift
+++ ChefsChoice/Modules/SearchModule/SearchVC.swift
@@ -24,22 +24,30 @@
 import UIKit
 import SwiftUI
 
-class SearchVC: UIViewController {
+class SearchVC: UIViewController, UIGestureRecognizerDelegate {
     
     
     //MARK: - UI Elements
     
     var filterStackView                    = UIStackView()
-    var filterTitleLabel                   = UILabel()
+    var filterTitleButton                  = UIButton()
+    //var filterTitleLabel                   = UILabel()
 
     var ratingStackView                    = UIStackView()
     var ratingTitleLabel                   = UILabel()
     var ratingButton                       = UIButton()
+    
+    var ratingButtonArray: [UIButton]      = []
     var applyButton                        = UIButton()
 
     var categoryStackView                  = UIStackView()
     var categoryTitleLabel                 = UILabel()
-    //var categoryGrid                     = UICollectionView()
+    var categoryGrid                       = UICollectionView(frame: .zero, collectionViewLayout: {
+    let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        return layout
+    }())
+    
     var gridPlaceholder                    = UIView()
     
     var cookingTimeStack                   = UIStackView()
@@ -67,14 +75,19 @@ class SearchVC: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         
+        categoryGrid.dataSource = self
+        categoryGrid.delegate = self
+        tap.delegate = self
     }
     
     
     //MARK: - UI Setup Methods
     
     func setupUI() {
-        
-        let titleArray = [filterTitleLabel: "Filter", ratingTitleLabel: "by Rating", categoryTitleLabel: "by Category", cookingTitleLabel: "by Total Cooking Time", healthyTypeTitleLabel: "by Type"]
+       
+        view.backgroundColor = .systemBackground
+        // removed [filterTitleLabel: "Filter",]  from array
+        let titleArray = [ratingTitleLabel: "by Rating", categoryTitleLabel: "by Category", cookingTitleLabel: "by Total Cooking Time", healthyTypeTitleLabel: "by Type"]
         
         for (key, value) in titleArray {
             configureTitleLabels(label: key, withTitle: value)
@@ -84,8 +97,10 @@ class SearchVC: UIViewController {
         setupFilterStackView()
         configureTableView()
         
-        filterTitleLabel.textAlignment = .center
-        filterTitleLabel.font = UIFont.systemFont(ofSize: 15, weight: .black)
+//        filterTitleLabel.textAlignment = .center
+//        filterTitleLabel.font = UIFont.systemFont(ofSize: 15, weight: .black)
+//        filterTitleLabel.isUserInteractionEnabled = true
+        //filterTitleLabel.addGestureRecognizer(tap)
     }
     
     
@@ -103,15 +118,19 @@ class SearchVC: UIViewController {
         filterStackView.isLayoutMarginsRelativeArrangement = true
         filterStackView.layoutMargins = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
         
+        //filterStackView.addGestureRecognizer(tap)
+        
         view.addSubview(filterStackView)
         
+        configureFilterTitleButton()
         setupRatingStackView()
         setupCategoryStackView()
         setupCookingTimeStackView()
         setupHealthyTypeStackView()
         
         
-        filterStackView.addArrangedSubview(filterTitleLabel)
+        //filterStackView.addArrangedSubview(filterTitleLabel)
+        filterStackView.addArrangedSubview(filterTitleButton)
         filterStackView.addArrangedSubview(ratingStackView)
                 //filterStackView.addArrangedSubview(ratingButton)
         filterStackView.addArrangedSubview(categoryStackView)
@@ -139,6 +158,19 @@ class SearchVC: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
     }
     
+    func configureFilterTitleButton() {
+        view.addSubview(filterTitleButton)
+        filterTitleButton.setTitle("Filter", for: .normal)
+        filterTitleButton.setTitleColor(.label, for: .normal)
+        
+        filterTitleButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        filterTitleButton.isUserInteractionEnabled = false
+        filterTitleButton.addTarget(self, action: #selector(hideShowFilterViews), for: .touchUpInside)
+//     
+//        filterTitleButton.layer.borderWidth = 1
+//        filterTitleButton.layer.borderColor = UIColor.black.cgColor
+    }
+    
     func сonfigureRatingButtons(for button: UIButton) {
         
         view.addSubview(button)
@@ -147,9 +179,6 @@ class SearchVC: UIViewController {
         button.setBackgroundImage(UIImage(systemName: "star.fill"), for: .selected)
         
         button.tintColor = .systemOrange
-    
-    
-      
         
         button.translatesAutoresizingMaskIntoConstraints = false
         
@@ -165,6 +194,8 @@ class SearchVC: UIViewController {
         
         applyButton.setTitle("Apply", for: .normal)
         applyButton.setTitleColor(.blue, for: .normal)
+        applyButton.addTarget(self, action: #selector(applyButtonTapped), for: .touchUpInside)
+        
         
         applyButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
@@ -190,21 +221,19 @@ class SearchVC: UIViewController {
         hStack.addArrangedSubview(applyButton)
         hStack.translatesAutoresizingMaskIntoConstraints = false
         
-        
-        
         let starStack = UIStackView()
         starStack.axis = .horizontal
         starStack.distribution = .equalSpacing
         view.addSubview(starStack)
         
-        
-        
     //TODO: find out how to better add buttons to stack
-        for _ in 1...5 {
+        for id in 1...5 {
             let newStarBTN = UIButton()
             сonfigureRatingButtons(for: newStarBTN)
+            newStarBTN.tag = id
             view.addSubview(newStarBTN)
             starStack.addArrangedSubview(newStarBTN)
+            ratingButtonArray.append(newStarBTN)
         }
         starStack.translatesAutoresizingMaskIntoConstraints = false
         
@@ -213,13 +242,25 @@ class SearchVC: UIViewController {
     }
     
     func configureGridView() {
-        view.addSubview(gridPlaceholder)
-        gridPlaceholder.backgroundColor = .gray
-        gridPlaceholder.translatesAutoresizingMaskIntoConstraints = false
+//        view.addSubview(gridPlaceholder)
+//        gridPlaceholder.backgroundColor = .gray
+//        gridPlaceholder.translatesAutoresizingMaskIntoConstraints = false
+//
+//        NSLayoutConstraint.activate([
+//            gridPlaceholder.widthAnchor.constraint(equalToConstant: 100),
+//            gridPlaceholder.heightAnchor.constraint(equalToConstant: 200)
+//        ])
+        
+        view.addSubview(categoryGrid)
+        
+        categoryGrid.register(CategoryCollectionViewCell.self, forCellWithReuseIdentifier: "gridCell")
+        //categoryGrid.backgroundColor = .black
+        
+        
+        categoryGrid.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            gridPlaceholder.widthAnchor.constraint(equalToConstant: 100),
-            gridPlaceholder.heightAnchor.constraint(equalToConstant: 200)
+            categoryGrid.heightAnchor.constraint(equalTo: categoryGrid.widthAnchor, multiplier: 0.5)
         ])
     }
     
@@ -230,13 +271,13 @@ class SearchVC: UIViewController {
 
         categoryStackView.axis = .vertical
         categoryStackView.addArrangedSubview(categoryTitleLabel)
-        categoryStackView.addArrangedSubview(gridPlaceholder)
+        //categoryStackView.addArrangedSubview(gridPlaceholder)
+        categoryStackView.addArrangedSubview(categoryGrid)
 
         //TODO: Add proper Grid
         categoryStackView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-           
             categoryStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
     }
@@ -257,7 +298,6 @@ class SearchVC: UIViewController {
     }
 
     func setupCookingTimeStackView() {
-
         configureCookingTimeLabel()
         configureCookingTimeSlider()
         
@@ -267,7 +307,7 @@ class SearchVC: UIViewController {
         cookingTimeStack.addArrangedSubview(cookingTitleLabel)
         cookingTimeStack.addArrangedSubview(cookingTimeSlider)
         cookingTimeStack.addArrangedSubview(cookingTimeLabel)
-        
+
     }
 
     func configureHealthyTypeButtons() {
@@ -288,6 +328,28 @@ class SearchVC: UIViewController {
     }
     
     
+    //MARK: - Button Methods
+    
+    @objc func applyButtonTapped() {
+        hideShowFilterViews()
+    }
+    
+    @objc func hideShowFilterViews() {
+        filterTitleButton.isUserInteractionEnabled.toggle()
+        UIView.animate(withDuration: 1, delay: 0.1) { [self] in
+            [ratingStackView, categoryStackView, cookingTimeStack, healthyTypeStackView].forEach {$0.isHidden.toggle()}
+//        } completion: { _ in
+//            UIView.animate(withDuration: 2, delay: 0.2) {[self] in
+//                [ratingStackView, categoryStackView, cookingTimeStack, healthyTypeStackView].forEach {$0.isHidden.toggle()}
+//            }
+        }
+    }
+    
+    let tap = UITapGestureRecognizer(target: SearchVC.self, action: #selector(handleTap))
+    
+    @objc func handleTap(_ sender: UITapGestureRecognizer) {
+         print("Hello World")
+      }
 }
 
 
@@ -295,13 +357,9 @@ class SearchVC: UIViewController {
 
 extension SearchVC: UITableViewDelegate, UITableViewDataSource {
     
-    
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         1
     }
-    
-    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? RecipeTableViewCell {
@@ -342,6 +400,29 @@ extension SearchVC: UITableViewDelegate, UITableViewDataSource {
     
 }
 
+//MARK: - CollectionView Setup
+
+extension SearchVC: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width/2, height: collectionView.frame.width/2)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "gridCell", for: indexPath) as! CategoryCollectionViewCell
+        cell.configureCell(categoryName: "Seafood", imageName: "seafood")
+        cell.layer.borderColor = UIColor.systemOrange.cgColor
+        cell.layer.borderWidth = 5
+        
+        return cell
+    }
+    
+    
+}
 
 //MARK: - SUI Preview
 
